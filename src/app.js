69475516b4ae64828838187b2a3b021d6dd00dc3
Jayme Coulton <coultonjayme@yahoo.com>
2021-10-21 16:04:46 -0500

    mod2 question9

--- src/app.js
+++ src/app.js
@@ -1,15 +1,44 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const { __ } = require("ramda");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
+
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render("index", { title: "Index" }));
+const accountData = fs.readFileSync(
+  path.join(__dirname, "json", "accounts.json"),
+  "utf8"
+);
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+  path.join(__dirname, "json", "users.json"),
+  "utf8"
+);
+const user = JSON.parse(userData);
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Account Summary", accounts });
+});
+
+app.get("/savings", (req, res) => {
+  res.render("account", { account: accounts.savings });
+});
+
+app.get("/checking", (req, res) => {
+  res.render("accounts", { account: accounts.checking });
+});
+
+app.get("/credit", (req, res) => {
+  res.render("accounts", { accounts: accounts.credit });
+});
+app.get("/profile", (req, res) => {
+  res.render("profile", { user: user[0] });
+});
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
