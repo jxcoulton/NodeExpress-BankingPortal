19dcd2db2f89daffe3bf397b237a7ee349f760e2
Jayme Coulton <coultonjayme@yahoo.com>
2021-10-21 19:46:10 -0500

    mod4 question10

--- src/app.js
+++ src/app.js
@@ -59,7 +59,27 @@ app.post("/transfer", (req, res) => {
     accountsJSON,
     "utf8"
   );
-  res.render("transfer", { message: "Transfer Completed" });
+  res.render("/transfer", { message: "Transfer Completed" });
+});
+
+app.get("/payment", (req, res) => {
+  res.render("payment", { account: accounts.credit });
+});
+
+app.post("/payment", (req, res) => {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount;
+  accounts.credit.available =
+    parseInt(accounts.credit.available) + parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, "json/accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+  res.render("/payment", {
+    message: "Payment Successful",
+    account: accounts.credit,
+  });
 });
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
